8e0d355784811defb5bead4d6027813d692b4df1
Sebastian Caicedo <jsebdev@gmail.com>
2023-04-08 22:14:21 -0500

    add app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hello negrito " + name + "!!"
 
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
